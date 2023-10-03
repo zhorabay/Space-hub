c35ed3f589e250e551431543c3eb005e6adccad9
Blessing <blessingmike334@gmail.com>
2023-10-03 12:38:35 +0100

    Add navbar title

--- src/features/rockets/rocketsSlice.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const rocketsSlice = createSlice({
-  name: 'rockets',
-  initialState: [],
-  reducers: {},
-});
-
-export default rocketsSlice.reducer;
