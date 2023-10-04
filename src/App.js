14cf848148695c2a31b1beb822ba6a17037e5fe1
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-04 11:39:24 +0300

    Add MyProfile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
+import MyProfile from './components/MyProfile';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route element={<Rockets />} path="/" />
         <Route element={<Missions />} path="/missions" />
+        <Route element={<MyProfile />} path="/myprofile" />
       </Routes>
     </main>
   );
---
+++ src/components/MyProfile.js
@@ -0,0 +1,46 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../styles/MyProfile.css';
+
+function MyProfile() {
+  const missionsData = useSelector((state) => state.missions.filter((mission) => mission.reserved));
+  const rocketsData = useSelector((state) => state.rockets.filter((rocket) => rocket.reserved));
+
+  return (
+    <section className="myprofile">
+      <div className="mymissions">
+        <h2>My Missions</h2>
+        {missionsData.length === 0 ? (
+          <div className="nomissions">
+            <p>No missions joined</p>
+            <Link className="join-missions-link" to="/missions">Join Missions</Link>
+          </div>
+        ) : (
+          <ul className="list">
+            {missionsData.map((mission) => (
+              <li key={mission.id} className="list-table">{mission.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="myrockets">
+        <h2>My Rockets</h2>
+        {rocketsData.length === 0 ? (
+          <div className="noreservations">
+            <p>No reservations made</p>
+            <Link className="make-reservetions-link" to="/">Make Reservations</Link>
+          </div>
+        ) : (
+          <ul className="list">
+            {rocketsData.map((rocket) => (
+              <li key={rocket.id} className="list-table">{rocket.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default MyProfile;
